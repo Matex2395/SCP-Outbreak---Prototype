41b51c5c366286434b21975cf321e79de414b4a6
Mateo Rosero <mateorgafjo@gmail.com>
2024-06-19 17:21:17 -0500

    Modelo 3D de personaje agregado
    Se ha agregado el modelo 3D del personaje y se le ha implementado el movimiento.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Tank Controller")]
+    [SerializeField] private float _walkSpeed;
+    [SerializeField] private float _runSpeedMultiplier;
+    [SerializeField] private float _backwardsSpeedMultiplier;
+    [SerializeField] private float _turnSpeed;
+    private float _currentSpeed; // Alternar entre caminar y correr
+
+    private CharacterController _characterController;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+
+        // El jugador empieza caminando
+        _currentSpeed = _walkSpeed;
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        Movement();
+        Rotation();
+    }
+
+    // Función para mover al personaje con controles tipo tanque
+    public void Movement()
+    {
+        var verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButton("Run") && verticalInput > 0)
+        {
+            // Correr hacia adelante
+            _currentSpeed = _walkSpeed * _runSpeedMultiplier;
+        }
+        else
+        {
+            // Validación para verificar si está caminando hacia atrás o hacia adelante
+            if (verticalInput < 0)
+            {
+                _currentSpeed = _walkSpeed * _backwardsSpeedMultiplier;
+            }
+            else
+            {
+                _currentSpeed = _walkSpeed;
+            }
+        }
+
+        var forwardVelocity = transform.forward * verticalInput * _currentSpeed * Time.deltaTime;
+
+        //Mover al personaje
+        _characterController.Move(forwardVelocity);
+
+    }
+
+    // Función para rotar el personaje
+    public void Rotation()
+    {
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        // Rotar al personaje
+        transform.Rotate(0, horizontalInput * _turnSpeed * Time.deltaTime, 0);
+    }
+}
